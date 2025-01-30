df348f816b4f961ddf59f5d340005f2e2c5d2f22
FerencSchmidt <ferenc_schmidt@epam.com>
2025-01-30 15:30:22 +0100

    Changed services and scripts

--- Kubernetes/Promtail/test_push.sh
+++ Kubernetes/Promtail/test_push.sh
@@ -1,3 +1,4 @@
+# Test pushing logs to loki
 kubectl run curl --image=curlimages/curl --rm -it --restart=Never --command -- /bin/sh -c "curl -XPOST -H 'Content-Type: application/json' http://loki:3100/loki/api/v1/push -d '{
   \"streams\": [
     {
@@ -10,3 +11,6 @@ kubectl run curl --image=curlimages/curl --rm -it --restart=Never --command -- /
     }
   ]
 }'"
+
+# List labels available 
+kubectl run temp-curl --rm -it --image=curlimages/curl --restart=Never --     curl -G -s http://loki:3100/loki/api/v1/labels
--- Kubernetes/Web/Docker/build.sh
+++ Kubernetes/Web/Docker/build.sh
@@ -1,1 +1,36 @@
-docker build -t webapp .
+#!/bin/bash
+CLUSTER_NAME="kind"
+
+# List loaded images from the KinD cluster
+docker exec -it ${CLUSTER_NAME}-control-plane crictl images
+
+# To delete a loaded image use this:
+# docker exec -it $(kind get clusters | head -1)-control-plane crictl rmi IMAGENAME
+
+echo "----------------------------------------------------------------------------------------------------------------------"
+
+# Ask user if they want to build the Docker image
+read -p "Do you want to build the Docker image? (y/n): " build_decision
+
+if [[ "$build_decision" =~ ^[Yy]$ ]]; then
+    # Ask for the version number
+    read -p "Enter the version number for the Docker image (e.g., 1.0.0): " version
+
+    # Build the Docker image with the specific tag
+    echo "Building Docker image with version: $version"
+    docker build -t webapp:$version .
+    echo "Build complete."
+
+    # Ask user if they want to load the image into KinD
+    read -p "Do you want to load the image into KinD? (y/n): " load_decision
+
+    if [[ "$load_decision" =~ ^[Yy]$ ]]; then
+        # Load the Docker image into the KinD cluster
+        kind load docker-image webapp:$version --name $CLUSTER_NAME
+        echo "Image loaded into KinD."
+    else
+        echo "Skipping image load into KinD."
+    fi
+else
+    echo "Skipping Docker build."
+fi
