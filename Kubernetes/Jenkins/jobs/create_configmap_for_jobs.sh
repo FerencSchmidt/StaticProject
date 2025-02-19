712afd6c9aaed87504fa1a2f4a60203ac25138e7
FerencSchmidt <ferenc_schmidt@epam.com>
2025-02-19 16:43:32 +0100

    updated configmap creation for jobs

--- Kubernetes/Jenkins/jobs/create_configmap_for_jobs.sh
+++ Kubernetes/Jenkins/jobs/create_configmap_for_jobs.sh
@@ -1,1 +1,19 @@
-kubectl create configmap jenkins-jobs --from-file=./library/ -n jenkins
+#!/bin/bash
+
+# Variables
+NAMESPACE=jenkins
+CONFIGMAP_NAME=jenkins-jobs
+DIRECTORY=./library/
+
+# Check if the ConfigMap exists
+if kubectl get configmap $CONFIGMAP_NAME -n $NAMESPACE; then
+    echo "ConfigMap $CONFIGMAP_NAME exists. Updating..."
+    # Deleting the old ConfigMap first (to be replaced with new data)
+    kubectl delete configmap $CONFIGMAP_NAME -n $NAMESPACE
+    # Create a new ConfigMap with the updated data
+    kubectl create configmap $CONFIGMAP_NAME --from-file=$DIRECTORY -n $NAMESPACE
+else
+    echo "ConfigMap $CONFIGMAP_NAME does not exist. Creating..."
+    # Create a new ConfigMap
+    kubectl create configmap $CONFIGMAP_NAME --from-file=$DIRECTORY -n $NAMESPACE
+fi
