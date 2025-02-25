af952ea2922d3a02baa08f22466e4cc34a5d96e8
FerencSchmidt <ferenc_schmidt@epam.com>
2025-02-25 17:32:24 +0100

    deployment script update

--- Kubernetes/Jenkins/jobs/scripts/job-build.sh
+++ Kubernetes/Jenkins/jobs/scripts/job-build.sh
@@ -9,12 +9,11 @@ VERSION_FILE="$WORKSPACE/Kubernetes/Web/Docker/version.txt"
 DEPLOYMENT_TEMPLATE_PATH="$WORKSPACE/Kubernetes/Web/web-deployment.yaml"
 MODIFIED_DEPLOYMENT_PATH="$WORKSPACE/Kubernetes/Web/modified_web-deployment.yaml"
 
-ls -lah $WORKSPACE/Kubernetes/Web/Docker
-
 # Check if the version file exists and read from it
 if [ -f "$VERSION_FILE" ]; then
     read -r version_tag < $VERSION_FILE
     echo "Version read from file: $version_tag"
+    export VERSION_TAG=$version_tag
 else
     echo "version.txt file not found. Failing the build."
     exit 1
@@ -23,10 +22,14 @@ fi
 # Substitute the version tag in the deployment template using envsubst
 VERSION_TAG=$version_tag envsubst '${VERSION_TAG}' < $DEPLOYMENT_TEMPLATE_PATH > $MODIFIED_DEPLOYMENT_PATH
 echo "Deployment file has been updated with new tag: $version_tag"
-ls -lah $WORKSPACE/Kubernetes/Web/
+cat $MODIFIED_DEPLOYMENT_PATH
 
 # Apply the deployment
 kubectl apply -f $MODIFIED_DEPLOYMENT_PATH
+if [ $? -ne 0 ]; then
+    echo "Failed to apply Kubernetes deployment"
+    exit 1
+fi
 echo "Kubernetes Deployment has been applied successfully."
 
 echo "======================== END SECTION: BUILD ========================"
